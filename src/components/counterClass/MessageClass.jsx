0d0e587aa53482cc3b57eb5e6d5c9785984cc178
analuisadev <analuisavidal89@gmail.com>
2023-07-29 20:20:04 -0300

    desafio 02 - introdução ao reactjs

---
+++ src/components/counterClass/MessageClass.jsx
@@ -0,0 +1,13 @@
+import  { Component } from 'react';
+
+export class MessageClass extends Component {
+  render() {
+    return <h1>Hello World!</h1>;
+  }
+}
+
+export class CustomMessageClass extends Component {
+  render() {
+    return <p>Eu sou o {this.props.message}</p>;
+  }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { GreetingFuncional } from './components/greeting/GreetingFuncional'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { GreetingFuncional } from './components/greeting/GreetingFuncional';
+import { MessageClass, CustomMessageClass } from './components/counterClass/MessageClass';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GreetingFuncional name="Rafaela"/>
-    <GreetingFuncional name="Marcos"/>
-  </React.StrictMode>,
-)
+    <GreetingFuncional name="Rafaela" />
+    <GreetingFuncional name="Marcos" />
+
+    <MessageClass />
+    <CustomMessageClass message="Marcos!" />
+  </React.StrictMode>
+);
