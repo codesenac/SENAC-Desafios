e620fe441867c535a3a7ae03c554d5d5fd93e859
analuisadev <analuisavidal89@gmail.com>
2023-07-29 20:23:33 -0300

    desafio 03 - introdução ao reactjs e renomeação de pastas

---
+++ src/components/desafio 03/Card.jsx
@@ -0,0 +1,9 @@
+const Card = ({ children }) => {
+  return (
+    <div style={{ border: '1px solid black', padding: '10px', margin: '10px', borderRadius: '5px' }}>
+      {children}
+    </div>
+  );
+};
+
+export default Card;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { GreetingFuncional } from './components/greeting/GreetingFuncional';
-import { MessageClass, CustomMessageClass } from './components/counterClass/MessageClass';
+import { GreetingFuncional } from './components/desafio 02/GreetingFuncional';
+import { MessageClass, CustomMessageClass } from './components/desafio 01/MessageClass';
+import Card from './components/desafio 03/Card';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -10,5 +11,17 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
     <MessageClass />
     <CustomMessageClass message="Marcos!" />
+
+    <div>
+      <Card>
+        <p>Mensagem 1</p>
+      </Card>
+      <Card>
+        <p>Mensagem 2</p>
+      </Card>
+      <Card>
+        <p>Mensagem 3</p>
+      </Card>
+    </div>
   </React.StrictMode>
 );
